a8aae7c4d99eb6b06d2fc6b7376e014394c928a7
joellegg <joellegg@bellsouth.net>
2017-05-31 15:49:52 -0500

    working through polynomial regression

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression_jal.py
@@ -0,0 +1,30 @@
+# Polynomial Regression
+
+# Data Preprocessing Template
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Because we have a small dataset we will use all of the data to create our model
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+# transforms X into multiple columns with x^2, x^3 etc.
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
